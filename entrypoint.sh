c47c69dc96b4e3a5f85d21e4bd3816daaccdcff5
Tayo <113843916+Oluty-1@users.noreply.github.com>
2025-03-03 13:18:44 +0100

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Parse APP_CONFIG and export variables
+# eval $(echo $APP_CONFIG | jq -r 'to_entries | map("export " + .key + "=\"" + (.value | tostring) + "\"") | .[]')
+eval $(echo $APP_CONFIG | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')
+
+# Execute the main application
+# exec ./apex_network apex_network_api
+
+# Run the main application
+exec "$@"
