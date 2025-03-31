0681eedccfc3e706b4665e964fbaf61aac3d5785
Oluty-1 <tayoefunshile@gmail.com>
2025-03-31 17:46:21 +0100

    final debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,3 @@
-# #!/bin/sh
-# set -e
-
-# # Parse ENV_VARS_JSON if it exists
-# if [ -n "$ENV_VARS_JSON" ]; then
-#   eval "$(echo "$ENV_VARS_JSON" | jq -r 'to_entries|map("export \(.key)=\(.value|tostring)")|.[]')"
-# fi
-
-# # Start application
-# exec "$@"
-
-
-
-
-
-
-
 #!/bin/sh
 set -e
 
