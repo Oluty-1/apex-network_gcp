c7dee2d1a59aa3a850ab704603f6da57267df032
Oluty-1 <tayoefunshile@gmail.com>
2025-03-31 10:36:01 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,25 @@
+# #!/bin/sh
+# set -e
+
+# # Parse ENV_VARS_JSON if it exists
+# if [ -n "$ENV_VARS_JSON" ]; then
+#   eval "$(echo "$ENV_VARS_JSON" | jq -r 'to_entries|map("export \(.key)=\(.value|tostring)")|.[]')"
+# fi
+
+# # Start application
+# exec "$@"
+
+
+
+
 #!/bin/sh
 set -e
 
-# Parse ENV_VARS_JSON if it exists
-if [ -n "$ENV_VARS_JSON" ]; then
-  eval "$(echo "$ENV_VARS_JSON" | jq -r 'to_entries|map("export \(.key)=\(.value|tostring)")|.[]')"
+# Check if the secrets file exists and load the environment variables
+if [ -f /mnt/secrets/secrets.json ]; then
+  echo "🔑 Loading secrets from /mnt/secrets/apexsecrets.json..."
+  eval "$(jq -r 'to_entries|map("export \(.key)=\(.value|tostring)")|.[]' /mnt/secrets/apexsecrets.json)"
 fi
 
-# Start application
+# Start the application
 exec "$@"
